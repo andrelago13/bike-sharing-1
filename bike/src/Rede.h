e0eb4f0776432748f661180c29650a7bf9f09b9c
leonardo <leonardo@leonardo-HP-ENVY-15-Notebook-PC>
2014-10-30 15:22:38 +0000

    Rede header file iniciated

---
+++ bike/src/Rede.h
@@ -0,0 +1,14 @@
+#include<vector>
+
+using namespace std;
+
+class Rede
+{
+public:
+	vector<Empresa> Empresas;
+	vector<* PostoServico> PostoServico;
+	vector<* Utilizador> Utilizadores;
+}
+
+
+#endif /* REDE_H_ */
