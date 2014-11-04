59ce0e6f9a5d329ddecf1f978e1ec2a05e065814
aslmbc13 <asl_mbc_13@hotmail.com>
2014-11-04 00:26:37 +0000

    Added more menus and exit function

--- bike/src/main.cpp
+++ bike/src/main.cpp
@@ -5,14 +5,22 @@
 
 using namespace std;
 
-int menu_start();
 int menu_system();
+#define MENU_exit -1
+int menu_start();
+#define MENU_start 0
+int menu_regUsr();
+#define MENU_regUsr 1
+int menu_ocUsr();
+#define MENU_ocUsr 2
+int menu_manager();
+#define MENU_manager 3
+void menu_exit_prog();
+
 void clear_screen();
 int str_to_int(string &str);
 void get_option(int &option, int min_range, int max_range);
-int menu_regUsr();
-int menu_ocUsr();
-int menu_manager();
+void print_menu_header();
 
 int main()
 {
@@ -21,6 +29,12 @@ int main()
 
 int menu_system()
 {
+	/* The system in this function manages the interactions between menus, i.e., the transitions between them
+
+	 Each menu is assigned a number, its ID. Every menu has a corresponding function. This function's return
+	 specifies which menu must be loaded next, according to the user's actions during it's execution. For example,
+	 if a menu function wishes to return to the main menu, it must return 0, main menu's ID.*/
+
 	int menu = 0;
 
 	while(menu >= 0)
@@ -43,8 +57,10 @@ int menu_system()
 			case 3:
 				menu = menu_manager();
 				break;
+			case -1:
+				break;
 			default:
-				menu = -1;
+				menu = MENU_exit;
 				break;
 			}
 		}
@@ -55,6 +71,8 @@ int menu_system()
 		}
 	}
 
+	menu_exit_prog();
+	system("pause");
 	return 0;
 }
 
@@ -74,25 +92,28 @@ int menu_start()
 	cout << "                                                              __/ |" << endl;
 	cout << "                                                             |___/ " << endl << endl;
 	cout << "   Welcome to our bike-sharing network!" << endl;
-	cout << endl << endl << "==> Please select your type of login:" << endl;
-	cout << "1 - Registered user" << endl;
-	cout << "2 - Occasional user" << endl;
-	cout << "3 - Management access" << endl;
+	cout << endl << endl << "===> Please select your type of login:" << endl;
+	cout << " 1 - Registered user" << endl;
+	cout << " 2 - Occasional user" << endl;
+	cout << " 3 - Management access" << endl;
+	cout << " 0 - Exit program" << endl;
 
 	int option;
-	get_option(option, 1, 3);
+	get_option(option, 0, 3);
 
 	switch (option)
 	{
 	case 1:
-		return 1;
+		return MENU_regUsr;
 	case 2:
-		return 2;
+		return MENU_ocUsr;
 	case 3:
-		return 3;
+		return MENU_manager;
+	case 0:
+		return MENU_exit;
 	}
 
-	return -1;
+	return MENU_exit;
 }
 
 int str_to_int(string &str)
@@ -141,7 +162,25 @@ void get_option(int &option, int min_range, int max_range)
 // TO-DO //
 int menu_regUsr()
 {
-	return 0;
+	// There must be a way for a user to login to the Rede
+	/*
+	print_menu_header();
+	cout << "===> Registered user secion" << endl << endl << "Select an option:" << endl;
+	cout << " 1 - Rent a bike" << endl;
+	cout << " 2 - Return a bike" << endl;
+	cout << " 3 - Report bike malfunction" << endl;
+	cout << " 0 - Return to previous menu" << endl;
+
+	int option;
+	get_option(option, 0, 5);
+
+	switch (option)
+	{
+	case 0:
+		return MENU_start;
+	}*/
+
+	return MENU_exit;
 }
 
 // TO-DO //
@@ -153,5 +192,43 @@ int menu_ocUsr()
 // TO-DO //
 int menu_manager()
 {
-	return 0;
+	print_menu_header();
+	cout << "===> Management section" << endl << endl << "Select an option:" << endl;
+	cout << " 1 - Supplying companies" << endl;
+	cout << " 2 - Service spots" << endl;
+	cout << " 3 - Bicicles" << endl;
+	cout << " 4 - Registered users" << endl;
+	cout << " 5 - Event logs" << endl;
+	cout << " 0 - Return to previous menu" << endl;
+
+	int option;
+	get_option(option, 0, 5);
+
+	switch (option)
+	{
+	case 0:
+		return MENU_start;
+	}
+
+	return MENU_exit;
+}
+
+void print_menu_header()
+{
+	cout << "      __          ___     __             __          __  " << endl;
+	cout << "     |__) | |__/ |__     /__` |__|  /\\  |__) | |\\ | / _`" << endl;
+	cout << "     |__) | |  \\ |___    .__/ |  | /~~\\ |  \\ | | \\| \\__>" << endl << endl;
+}
+
+void menu_exit_prog()
+{
+	clear_screen();
+	cout << endl << endl <<  "           _____                 _     ____" << endl;
+	cout << "          / ____|               | |   |  _ \\" << endl;
+	cout << "         | |  __  ___   ___   __| |   | |_) |" << endl;
+	cout << "         | | |_ |/ _ \\ / _ \\ / _` |   |  _ <| | | |/ _ \\" << endl;
+	cout << "         | |__| | (_) | (_) | (_| |   | |_) | |_| |  __/" << endl;
+	cout << "          \\_____|\\___/ \\___/ \\__,_|   |____/ \\__, |\\___|" << endl;
+	cout << "                                             __/ |     " << endl;
+	cout << "                                            |___/      " << endl << endl << endl << endl;
 }
